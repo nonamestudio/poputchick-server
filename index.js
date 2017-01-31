cc17a535f5ee15ea3ebaa599c52b26b325599481
abaikirov <abaikirov@outlook.com>
2017-01-31 21:57:30 +0600

    basic structure

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response){
+   response.send("poputchick-server"); 
+});
