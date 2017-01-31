06077811c1db1d7ee027f97ea6e9e462698601a6
abaikirov <abaikirov@outlook.com>
2017-01-31 22:03:28 +0600

    app.listen

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function(request, response){
    response.send("poputchick-server"); 
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
